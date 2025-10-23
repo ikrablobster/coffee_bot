99bc89105bdcde2693db407805a94202236744da
Dmytro <149416064+ikrablobster@users.noreply.github.com>
2025-10-23 12:43:13 +0300

    Update coffee_bot.py

--- coffee_bot.py
+++ coffee_bot.py
@@ -116,10 +116,11 @@ conv = ConversationHandler(
         fallbacks=[CommandHandler("cancel", cancel)],
     )
 
-    app.add_handler(conv)
-    app.run_polling()
+app.add_handler(conv)
+app.run_polling()
 
 if __name__ == "__main__":
 
     main()
+
 
